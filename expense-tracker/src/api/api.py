ddc6e5d345019ca1f0278831921b91f6b7cc3674
Marius <mariusklumbys@gmail.com>
2025-01-11 23:45:37 +0200

    Update files

--- expense-tracker/src/api/api.py
+++ expense-tracker/src/api/api.py
@@ -1,6 +1,23 @@
 from fastapi import FastAPI
+from models import User, Expense
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
-@app.get("/")
-def root():
+test_user = User("Test User", "abc123")
+
+@app.get("/expenses")
+def get_expenses():
+    return test_user.get_expenses()
+
+@app.post("/expense")
+def add_expense(expense : Expense):
+    test_user.add_expense(expense)
+    return {"message": "Expense added successfully!"}
